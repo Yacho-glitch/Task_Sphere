19d975642bcad181fef26eba322ff0e31675be76
Yacho-glitch <yahyabousksou@gmail.com>
2026-04-24 19:47:20 +0100

    feat: initialize laravel & tailwindcss environment

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Task Sphere</title>
+
+        @vite('resources/css/app.css')
+
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold text-blue-600">
+            Hello Tailwind!
+        </h1>
+    </body>
+</html>

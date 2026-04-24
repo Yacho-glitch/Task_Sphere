6044cdc9e451b8fb9250b21d217302f6e4545890
Yacho-glitch <yahyabousksou@gmail.com>
2026-04-24 21:02:54 +0100

    feat: create factory & seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Task;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +18,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        Category::factory(5)->create();
+        Task::factory(30)->create();
 
         User::factory()->create([
             'name' => 'Test User',

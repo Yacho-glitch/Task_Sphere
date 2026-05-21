154668071a3bca5ad1aedc5e105d63adf7f9e951
Yacho-glitch <yahyabousksou@gmail.com>
2026-05-21 22:47:16 +0100

    feat: add the store method to a controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     public function showRegister() {
         return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        $validateData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|string|max:255',
+            'password|string|min:8|confirmed'
+        ]);
+
+        $user = User::create($request->all());
+
+
     }
 }

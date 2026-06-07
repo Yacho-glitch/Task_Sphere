1ebed9bccbe7fce90c5098e8bd34c39e53a1aa18
Yacho-glitch <yahyabousksou@gmail.com>
2026-06-07 15:34:45 +0100

    feat: add showLogin to login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function showLogin() {
+        return view('auth.login');
+    }
 }

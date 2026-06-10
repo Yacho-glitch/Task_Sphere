02b8e79c22730d62c4d0673c6e1428db74fbc436
Yacho-glitch <yahyabousksou@gmail.com>
2026-06-10 20:33:15 +0100

    add psw for validation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,6 +15,7 @@ class LoginController extends Controller
     public function authenticate(Request $request) {
         $credentials = $request->validate([
             'email' => 'required|email|string',
+            'password' => 'required|string'
         ]);
     }
 }

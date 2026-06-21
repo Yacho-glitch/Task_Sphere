75d4696411e491a408517143836c357d34651fa5
Yacho-glitch <yahyabousksou@gmail.com>
2026-06-21 19:20:32 +0100

    hh

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -33,5 +33,7 @@ class LoginController extends Controller
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }

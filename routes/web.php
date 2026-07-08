b3f8b125a140ba3f2898efb98c93e68a6db167f5
Yacho-glitch <yahyabousksou@gmail.com>
2026-07-08 16:11:46 +0100

    piopi

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/tasks/create', [DashboardController::class, 'create'])->name('create');
     Route::post('/tasks', [DashboardController::class, 'store']);
+    Route::get('/tasks/{task}/edit', [DashboardController::class, 'edit'])->name('tasks.edit');
     
 });
 
 
 
-Route::get('/tasks/{task}/edit', [DashboardController::class, 'edit'])->name('tasks.edit');
 
 Route::put('/tasks/{task}', [DashboardController::class, 'update'])->name('tasks.update');
 
